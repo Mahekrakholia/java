8f1da21d357266deaafda8fe059ae78d038506f5
Mahekrakholia <109951442+Mahekrakholia@users.noreply.github.com>
2022-08-04 23:51:00 -0700

    Add files via upload

---
+++ Str.java
@@ -0,0 +1,16 @@
+public class Str {
+
+    public static void main(String[] args) {
+   //NOTE: ****** Take values from command line*****
+   //This is just sample program. Use command line argument
+   // Always follow AIM of the program
+
+        String style = new String("Bold");
+        String style2 = new String("Bold");
+
+        if(style.equals(style2))
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+}
